b776cdda067fe7f4be6049a876c2f76d8eff0983
Adir Shaban <adirshaban1995@gmail.com>
2017-12-24 17:31:04 +0200

    stores the accesskey at the app state now

--- web/src/components/App/index.js
+++ web/src/components/App/index.js
@@ -3,10 +3,24 @@ import './App.css';
 import Navbar from '../Navbar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.authenticate = this.authenticate.bind(this);
+    this.state = {
+      user: {}
+    }
+  }
+
+  authenticate = (response) => {
+    this.setState({
+      user: response
+    });
+  }
+
   render() {
     return (
       <div className="App">  
-        <Navbar />
+        <Navbar authenticate={this.authenticate} user={this.state.user.name ? this.state.user : null} />
       </div>
     );
   }
--- web/src/components/Navbar/index.jsx
+++ web/src/components/Navbar/index.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './navbar.css';
 import wait from './wait.svg';
 import FacebookLogin from 'react-facebook-login';
+
+const Avatar = ({name, pic}) => (
+  <div>
+    <figure>
+      <img src={pic.data.url} alt={name} className="avatar-pic"/>
+    </figure>    
+    <span className="avatar-text">Hi, {name}</span>
+  </div>
+)
 
 
 class Navbar extends Component {
@@ -11,9 +21,10 @@ class Navbar extends Component {
   }
  responseFacebook = (response) => {
     console.log(response);
-  }
-  
-  render() {    
+    this.props.authenticate(response);
+  }  
+  render() {        
+    const user = this.props.user;
     return (
       <nav className="navbar is-fixed-top">
           <div className="navbar-brand">
@@ -26,21 +37,28 @@ class Navbar extends Component {
               <a className="navbar-item">About</a>
             </div>                        
             <div className="navbar-end">
-              <div className="navbar-item">
-              <FacebookLogin
-              appId={process.env.REACT_APP_FACEBOOK_ID}
-              autoLoad
-              fields="name,email,picture"
-              callback={this.responseFacebook}
-              cssClass="auth-button"
-              icon={<i className="fa fa-facebook"></i>}
-            />
+            <div className="navbar-item">
+              {user ?
+              <Avatar name={user.name} pic={user.picture} /> :                               
+                <FacebookLogin
+                  appId={process.env.REACT_APP_FACEBOOK_ID}
+                  autoLoad
+                  fields="name,email,picture.width(300).height(300)"
+                  callback={this.responseFacebook}
+                  cssClass="auth-button"
+                  icon={<i className="fa fa-facebook"></i>}
+                />
+              }
               </div>
             </div>
           </div>
       </nav>
     );
   }
+}
+
+Navbar.propTypes = {
+  authenticate: PropTypes.func
 }
 
 export default Navbar;
