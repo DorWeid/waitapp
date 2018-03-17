b43a96e0b8eca342c9d475f6c242f8d5b99f866f
DorWeid <dorprozor@gmail.com>
2018-03-17 20:28:38 +0200

    resolved some warnings

--- web/src/components/CategoryPopUp/index.js
+++ web/src/components/CategoryPopUp/index.js
@@ -1,16 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import img from "../../pictures/1.jpeg";
 import "./CategoryPopUp.css";
 
-export default class Popup extends React.Component {
-    render() {
-      return (  
-        <div className='popup'>
-          <div className='popup_inner'>
+export default class Popup extends React.Component {
+  render() {
+    return (
+      <div className="popup">
+        <div className="popup_inner">
           {/* <button onClick={this.props.closePopup}>close me</button> */}
-          <a href='/lists'><img className='category_image' src={img}/></a>
-          </div>
+          <a href="/lists">
+            <img
+              className="category_image"
+              src={img}
+              alt="thisShouldDefineEachCategoryWhenMapped"
+            />
+          </a>
         </div>
-      );
-    }
+      </div>
+    );
   }
+}
