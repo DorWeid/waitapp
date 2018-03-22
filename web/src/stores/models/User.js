95dfcec3d8c56c3534c936c6f5bfcc6de9a6497c
DorWeid <dorprozor@gmail.com>
2018-03-22 21:25:33 +0200

    a better solution

--- web/src/stores/models/User.js
+++ web/src/stores/models/User.js
@@ -2,11 +2,13 @@ import { types, getParent, flow } from "mobx-state-tree";
 
 // NOTE: Refer to Item.js model for explanation
 
-const LOCAL_STORAGE_PROFILE_IMG_URL = "imgUrl";
-const LOCAL_STORAGE_PROFILE_USER_NAME = "username";
-const LOCAL_STORAGE_PROFILE_EMAIL = "email";
-const LOCAL_STORAGE_PROFILE_CREATED_AT = "createdAt";
-const LOCAL_STORAGE_PROFILE_USER_MONGO_ID = "_id";
+const LOCAL_STORAGE_KEYS = {
+  LOCAL_STORAGE_PROFILE_IMG_URL: "picUrl",
+  LOCAL_STORAGE_PROFILE_USER_NAME: "username",
+  LOCAL_STORAGE_PROFILE_EMAIL: "email",
+  LOCAL_STORAGE_PROFILE_CREATED_AT: "createdAt",
+  LOCAL_STORAGE_PROFILE_USER_MONGO_ID: "_id"
+};
 
 const definition = {
   _id: types.identifier(types.string),
@@ -22,22 +24,18 @@ const views = self => {
       return getParent(self, 2);
     },
     get isUserAuthenticated() {
-      return (
-        localStorage.getItem(LOCAL_STORAGE_PROFILE_IMG_URL) &&
-        localStorage.getItem(LOCAL_STORAGE_PROFILE_EMAIL) &&
-        localStorage.getItem(LOCAL_STORAGE_PROFILE_USER_NAME) &&
-        localStorage.getItem(LOCAL_STORAGE_PROFILE_CREATED_AT) &&
-        localStorage.getItem(LOCAL_STORAGE_PROFILE_USER_MONGO_ID)
+      return Object.keys(LOCAL_STORAGE_KEYS).every(key =>
+        localStorage.getItem(LOCAL_STORAGE_KEYS[key])
       );
     },
     get getUserAuthDataFromStorage() {
-      return {
-        username: localStorage.getItem(LOCAL_STORAGE_PROFILE_USER_NAME),
-        picUrl: localStorage.getItem(LOCAL_STORAGE_PROFILE_IMG_URL),
-        email: localStorage.getItem(LOCAL_STORAGE_PROFILE_EMAIL),
-        createdAt: localStorage.getItem(LOCAL_STORAGE_PROFILE_CREATED_AT),
-        _id: localStorage.getItem(LOCAL_STORAGE_PROFILE_USER_MONGO_ID)
-      };
+      return Object.keys(LOCAL_STORAGE_KEYS).reduce((acc, key) => {
+        acc[LOCAL_STORAGE_KEYS[key]] = localStorage.getItem(
+          LOCAL_STORAGE_KEYS[key]
+        );
+
+        return acc;
+      }, {});
     }
   };
 };
@@ -54,13 +52,12 @@ const actions = self => {
     try {
       const response = yield self.store.post(url, options);
 
-      const { email, username, createdAt, _id } = response.data.user;
-
-      localStorage.setItem(LOCAL_STORAGE_PROFILE_EMAIL, email);
-      localStorage.setItem(LOCAL_STORAGE_PROFILE_USER_NAME, username);
-      localStorage.setItem(LOCAL_STORAGE_PROFILE_CREATED_AT, createdAt);
-      localStorage.setItem(LOCAL_STORAGE_PROFILE_IMG_URL, picUrl);
-      localStorage.setItem(LOCAL_STORAGE_PROFILE_USER_MONGO_ID, _id);
+      Object.keys(LOCAL_STORAGE_KEYS).forEach(function(key) {
+        localStorage.setItem(
+          LOCAL_STORAGE_KEYS[key],
+          this[LOCAL_STORAGE_KEYS[key]]
+        );
+      }, Object.assign({ picUrl }, response.data.user));
 
       return response.data.user;
     } catch (e) {
