c7263a282ebac5a7438f26dee8676ca4fccfd9a4
Liza Gulitski <liza.in.2009@gmail.com>
2018-03-23 19:17:15 +0300

    changed categories popover

--- web/src/components/CategoryPopUp/index.js
+++ web/src/components/CategoryPopUp/index.js
@@ -5,17 +5,15 @@ import "./CategoryPopUp.css";
 export default class Popup extends React.Component {
   render() {
     return (
-      <div className="popup">
-        <div className="popup_inner">
-          {/* <button onClick={this.props.closePopup}>close me</button> */}
-          <a href="/lists">
-            <img
-              className="category_image"
-              src={img}
-              alt="thisShouldDefineEachCategoryWhenMapped"
-            />
-          </a>
-        </div>
+      <div className="popup_inner">
+        {/* <button onClick={this.props.closePopup}>close me</button> */}
+        <a href="/lists">
+          <img
+            className="category_image"
+            src={img}
+            alt="thisShouldDefineEachCategoryWhenMapped"
+          />
+        </a>
       </div>
     );
   }
