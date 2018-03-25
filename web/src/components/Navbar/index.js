1629c5f31088ffe533ccc5bf7c3319fd5f21e3c6
Adir Shaban <adirshaban1995@gmail.com>
2018-03-25 18:37:53 +0300

    forgot to add link in navbar

--- web/src/components/Navbar/index.js
+++ web/src/components/Navbar/index.js
@@ -89,6 +89,15 @@ class Navbar extends Component {
                 <CategoryPopUp />
               </Popover>
             </div>
+            {
+              userStore.currentUser.admin ? (
+                <Link to="/pendingLists">
+                  <div className="navbar-custom-menuitem">
+                    Pending Lists                    
+                  </div>
+                </Link>
+              ) : null
+            }
             {userStore.isUserLoggedIn ? (
               <Link to={`/${userStore.currentUser._id}/profile`}>
                 <UserAvatar {...userStore.currentUser} />
