7fc03c8e2cbf6191ae8749111b263409f748ad80
Adir Shaban <adirshaban1995@gmail.com>
2018-04-16 19:11:59 +0300

    changed the lists page so it would be groupon

--- web/src/containers/Lists/index.js
+++ web/src/containers/Lists/index.js
@@ -1,81 +1,8 @@
 import React, { Component } from "react";
 import { observer, inject } from "mobx-react";
 import { Link } from "react-router-dom";
-import Item from "../../components/Item";
+import ItemList from "../../components/ItemList";
 import "./Lists.css";
-//import classnames from "classnames";
-
-// const items = [
-//   {
-//     name: "Zimmer in Holon",
-//     img: "http://img.mako.co.il/2015/11/17/hamelech_david_i.jpg",
-//     description: "Ahlen zimmer in holon",
-//     price: 550,
-//     currency: "usd",
-//     location: "Holon"
-//   },
-//   {
-//     name: "What is Holon",
-//     img: "http://img.mako.co.il/2015/11/17/hamelech_david_i.jpg",
-//     description: "Ahlen zimmer in holon",
-//     price: 550,
-//     currency: "usd",
-//     location: "Holon"
-//   },
-//   {
-//     name: "FUN in Holon",
-//     img: "http://www.nofshim.co.il/images/site_images1903X735/ezel_nano.jpg",
-//     description: "Ahlen zimmer in holon",
-//     price: 550,
-//     currency: "usd",
-//     location: "Holon"
-//   },
-//   {
-//     name: "Zimmer in Aciaent Holon",
-//     img:
-//       "https://www.safarihotelsnamibia.com/wp-content/uploads/2014/11/Safari-Court-Hotel-Pool.jpg",
-//     description: "Ahlen zimmer in Acient Holon",
-//     price: 650,
-//     currency: "ils",
-//     location: "Acient Holon"
-//   },
-//   {
-//     name: "Zimsmer in Acient Holon",
-//     img:
-//       "https://www.safarihotelsnamibia.com/wp-content/uploads/2014/11/Safari-Court-Hotel-Pool.jpg",
-//     description: "Ahlen zimmer in Acient Holon",
-//     price: 650,
-//     currency: "ils",
-//     location: "Acient Holon"
-//   },
-//   {
-//     name: "Zimmersin Acient Holon",
-//     img:
-//       "https://www.safarihotelsnamibia.com/wp-content/uploads/2014/11/Safari-Court-Hotel-Pool.jpg",
-//     description: "Ahlen zimmer in Acient Holon",
-//     price: 650,
-//     currency: "ils",
-//     location: "Acient Holon"
-//   },
-//   {
-//     name: "Zimmer in Ascient Holon",
-//     img:
-//       "https://www.safarihotelsnamibia.com/wp-content/uploads/2014/11/Safari-Court-Hotel-Pool.jpg",
-//     description: "Ahlen zimmer in Acient Holon",
-//     price: 650,
-//     currency: "ils",
-//     location: "Acient Holon"
-//   },
-//   {
-//     name: "Zimmer in Acient Holosn",
-//     img:
-//       "https://www.safarihotelsnamibia.com/wp-content/uploads/2014/11/Safari-Court-Hotel-Pool.jpg",
-//     description: "Ahlen zimmer in Acient Holon",
-//     price: 650,
-//     currency: "ils",
-//     location: "Acient Holon"
-//   }
-// ];
 
 class Lists extends Component {
   componentDidMount() {
@@ -94,25 +21,20 @@ class Lists extends Component {
         {items.size !== 0 &&
           categories.map(category => (
             <div className="item-list-category" key={category.en}>
-              <div className="item-list-category-title">
-                {category.en}{" "}
+              <div className="item-list-category-container">
                 <Link
-                  className="item-list-category-subtitle"
+                  className="item-list-category-title"
                   to={`/list/type/${category.en}`}
                 >
-                  view all{" "}
-                  <span className="icon">
-                    <i className="fas fa-arrow-right" />
-                  </span>
+                  {category.en}
                 </Link>
               </div>
-              <div className="item-list">
-                {items
+              <ItemList
+                items={items
                   .values()
                   .filter(item => item.type === category.en)
-                  .slice(0, size)
-                  .map(item => <Item key={item._id} {...item} />)}
-              </div>
+                  .slice(0, size)}
+              />
             </div>
           ))}
       </div>
